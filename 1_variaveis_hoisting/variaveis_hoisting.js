8d454a8f13d224be3813df3701d09bf109211215
Douglas Lima <douglasproglima@gmail.com>
2017-02-19 20:58:45 -0300

    1 - Hoinsting

---
+++ 1_variaveis_hoisting/variaveis_hoisting.js
@@ -0,0 +1,22 @@
+/**
+ * Created by Douglasproglima on 19/02/2017.
+ */
+/*HOISING
+ Leva todas as declarações de variáveis para o início do documento;
+ Você pode declarar uma variável no final do arquivo e adicionar um valor as variáveis no início do documento.
+
+ Obs: O Hoistung leva apenas a declaração da variável.
+ Boas práticas, é recomendavél declarar as variáveis no início do arquivo.
+ */
+
+percentualImposto = 0.30;
+vlr = 205.59
+totalImposto = percentualImposto * vlr;
+
+console.log(totalImposto);
+
+
+
+var vlr;
+var percentualImposto;
+var totalImposto;
