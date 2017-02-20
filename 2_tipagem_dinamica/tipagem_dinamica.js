ec226a8dbe484469029aba600a170629674a5003
Douglas Lima <douglasproglima@gmail.com>
2017-02-19 21:41:15 -0300

    3 - Tipagem Dinânica

---
+++ 2_tipagem_dinamica/tipagem_dinamica.js
@@ -0,0 +1,22 @@
+/**
+ * Created by Douglasproglima on 19/02/2017.
+ */
+var idade = 29
+console.log(typeof idade);
+
+idade = true;
+console.log(typeof idade);
+
+idade = 25;
+anos = idade + ' Anos';
+console.log(anos);
+console.log(typeof anos);
+
+var dataNasc = 25/12/1890;
+console.log(typeof dataNasc);
+
+if(!dataNasc){
+	console.log('IF');
+}else{
+	console.log('ELSE');
+}
